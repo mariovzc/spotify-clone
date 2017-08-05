2b367f31300a84de4700cff955eec5937e9e99db
¨Mario <mario.vizcainos@gmail.com>
2017-08-05 12:58:54 -0500

    added prefix to the app route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get 'artists', to: 'artists#index'
 
-  get 'artists/:id', to: 'artist#detail'
+  get 'artists/:id', to: 'artist#detail', as: 'artists_albums'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
